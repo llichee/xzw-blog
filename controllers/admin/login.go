d79084594231b9e649d73423ad2804e39583109a
zhangsan <13102133719@163.com>
2021-03-09 21:57:23 +0800

    添加验证码

--- controllers/admin/login.go
+++ controllers/admin/login.go
@@ -1,6 +1,20 @@
 package admin
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/cache"
+	"github.com/astaxie/beego/utils/captcha"
+)
+
+var cpt *captcha.Captcha
+
+func init() {
+	store := cache.NewMemoryCache()
+	cpt = captcha.NewWithFilter("/captcha/", store)
+	cpt.ChallengeNums = 4
+	cpt.StdWidth = 100
+	cpt.StdHeight = 40
+}
 
 type LoginController struct {
 	beego.Controller
